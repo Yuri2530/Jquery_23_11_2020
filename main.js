fd592af1bf29ae0d756920da4b0fa49b265a28e7
Yuri Sanguino Rodriguez <=>
2020-11-23 20:40:12 -0500

    Jquery

---
+++ main.js
@@ -0,0 +1,63 @@
+/*Declaración de variable ya con el valor*/
+/*
+var vaso="agua";
+console.log(vaso);
+
+/*Declarar variable y más adelante se le asigna el dato*/
+/*
+var vaso
+vaso= "jugo";
+console.log(vaso);
+
+vaso= "coca cola";
+console.log(vaso);
+
+*/
+
+/*
+let vaso, nombre= "Yuri" , apellido, edad;
+
+
+apellido = "Sanguino Rodriguez";
+vaso ="jugo";
+edad ="30"
+
+console.log(vaso,nombre, apellido, edad);
+*/
+
+let nombre_string="Yuri";
+let edad_number=30;
+let genero_femenino_boolean = true
+
+console.log(typeof(nombre_string), nombre_string);
+console.log(typeof(edad_number), edad_number);
+console.log(typeof(genero_femenino_boolean), genero_femenino_boolean);
+
+
+let nombre="Yuri";
+let apellido="Sanguino Rodriguez";
+let edad=30;
+let genero="Femenino";
+let email="sanguino.yuri@gmail.com";
+let vestido="llevo vestido de líneas"
+let cuello_lleva_accesorio = true;
+let color_ojos="cafe oscuro";
+let altura=1.62;
+let hobbie="escuchar música";
+
+console.log("Mi nombre es: " +nombre);
+console.log("Mis apellidos son: " +apellido);
+console.log("Mi edad es: "+edad_number);
+console.log("Mi genero es: "+edad_number);
+console.log("Mi correo es: "+email);
+console.log("Vesturio: "+vestido);
+console.log("Lleva algo en el cuello?: "+cuello_lleva_accesorio);
+console.log("Mi color de ojos es: "+color_ojos);
+console.log("Mi altura es: "+altura+" cm");
+console.log("Cuál es su hobbie: "+hobbie);
+
+
+
+
+
+
